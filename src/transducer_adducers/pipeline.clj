c93e19c50fae3c6aba7d1aa2f0cac04db81ab475
tkocmathla <matt.t.grimm@gmail.com>
2017-05-23 21:38:13 -0600

    Cleanup

--- src/transducer_adducers/pipeline.clj
+++ src/transducer_adducers/pipeline.clj
@@ -9,25 +9,11 @@
     [hickory.select :as hs :refer [select tag]]))
 
 (def ncpus (.availableProcessors (Runtime/getRuntime)))
-
-(defn pipeline-process [xform xs]
-  (let [cin (to-chan xs) cout (chan)]
-    (pipeline (* ncpus 8) cout xform cin)
-    (<!! (async/reduce conj [] cout))))
+(def adv-url "https://api.github.com/repos/richardwilkes/gcs_library/contents/Library/Advantages")
 
 ;; -----------------------------------------------------------------------------
 
-(def adv-url "https://api.github.com/repos/richardwilkes/gcs_library/contents/Library/Advantages")
-
-(def xform (comp (map keywordize-keys) 
-                 (map :download_url)
-                 (map http/get)
-                 (filter (comp #{200} :status))
-                 (keep :body)
-                 (map hc/parse)
-                 (map hc/as-hickory)
-                 (mapcat (partial select (hs/and (tag :advantage))))))
-
+;; reducers partition the collection and transform the partitions in parallel
 (defn reducer [coll]
   (->> coll
        (r/map keywordize-keys)
@@ -37,8 +23,25 @@
        (r/map :body)
        (r/map hc/parse)
        (r/map hc/as-hickory)
-       (r/mapcat (partial select (hs/and (tag :advantage))))
+       (r/mapcat (partial select (tag :advantage)))
        (r/fold 1 r/cat r/append!)))
+
+(def xform
+  (comp (map keywordize-keys)
+        (map :download_url)
+        (map http/get)
+        (filter (comp #{200} :status))
+        (keep :body)
+        (map hc/parse)
+        (map hc/as-hickory)
+        (mapcat (partial select (tag :advantage)))))
+
+(defn pipeline-process [xform xs]
+  (let [cin (to-chan xs) cout (chan)]
+    (pipeline (* ncpus 8) cout xform cin)
+    (<!! (async/reduce conj [] cout))))
+
+;; -----------------------------------------------------------------------------
 
 (defn time-these [] 
   (let [files (->> adv-url http/get :body json/read-str)]
